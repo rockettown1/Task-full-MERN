82eaa4b5b5244562741e9b590e277f605b5660cb
Dan Krishnan <dan@wearecodenation.com>
2020-03-30 00:18:33 +0100

    updated origin

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3004;
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://priceless-bell-a33de5.netlify.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
