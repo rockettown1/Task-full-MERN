dcb96d807b1e5ff7876ecdd7c4bc7da0a79c186d
Dan Krishnan <48166152+rockettown1@users.noreply.github.com>
2020-03-30 23:14:00 +0100

    updated

--- src/models/user.js
+++ src/models/user.js
@@ -56,7 +56,7 @@ userSchema.methods.toJSON = function() {
 
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, "thisismydemoapp");
+  const token = jwt.sign({ _id: user._id.toString() }, [SECRET]);
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
